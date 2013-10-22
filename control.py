0a71f7d0f1d50baeb5c0cfc44bdf492cc4ec077b
webgovernor <webgovernor@gmail.com>
2013-10-22 00:54:43 -0600

    Renamed to mobile.html

--- control.py
+++ control.py
@@ -59,8 +59,8 @@ def cache_template(tpl, minify=False):
         html = minify_template(html)
     open(out,'wb').write(html)
 
-def cache_phonegap(minify=False):
-    pg_tpl = env.get_template('phonegap.html')
+def cache_mobile(minify=False):
+    pg_tpl = env.get_template('mobile.html')
     out = os.path.join(self_path, const.PG_DOCROOT, 'index.html')
     print 'Caching PG template to %s'%(out)
     html = pg_tpl.render(version=const.VERSION)
@@ -73,7 +73,7 @@ def cache_all():
     ''' Update this list when new templates are added '''
     cache_template('index.html', minify=True)
     cache_template('thanks.html')
-    cache_phonegap(minify=True)
+    cache_mobile(minify=True)
     cache_version()
 
 def run_test_server():
